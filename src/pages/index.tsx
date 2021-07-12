688ec9e225538a99d927bf62b3d6461f994521bb
acatzk <joshuaimalay@gmail.com>
2021-07-12 21:08:25 +0800

    🎉 INT COMMIT: Next and Tailwindcss setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { NextPage } from 'next'
+
+const Index: NextPage<{}> = () => {
+  return <div className="text-2xl font-bold text-indigo-600">Hello Jeorgy</div>
+}
+
+export default Index
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
