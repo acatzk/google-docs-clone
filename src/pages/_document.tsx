4eabe91db5bac429cc74c708a325a052f59075cd
acatzk <joshuaimalay@gmail.com>
2021-07-13 13:17:53 +0800

    🚸 IMPROVE: Add default google font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ class Document extends NextDocument<{}> {
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet"
           />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
         <body>
           <Main />
